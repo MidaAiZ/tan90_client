efdb25293c423132b26ea81aedc71b8532db7650
Wu Mingda <1036570699@qq.com>
2017-07-22 00:29:56 +0800

    添加前台课程分页功能，修改课程列表文件命名

--- view/js/search-result.js
+++ view/js/search-result.js
@@ -1,43 +1,7 @@
 $(function() {
 
-    //连接服务器获取课程列表
-    var params = "limit=20&page=1";
-    var url = "http://115.159.188.200:8000/get_courses/"; 
-    
-    $.ajax({  
-            type: 'POST',  
-            url: url,  
-            dataType: 'json',  
-            data: params,
-            //下面2个参数用于解决跨域问题  
-            xhrFields: {
-                withCredentials: true
-            },
-            crossDomain: true,
-
-            complete: function(XMLHttpRequest, textStatus) { 
-            },  
-            success: function(data) {  
-                console.log(data);
-                var courses = data.courses;
-                for (var i=0; i<courses.length; i++) {
-                    $($(".sed-md")[i]).css("display", "inline");
-                    $($(".class-name")[i]).text(courses[i].name);
-                    $($(".class-name")[i]).attr("course-id",courses[i].id);
-                    $($(".class-pic")[i]).attr("course-id",courses[i].id);
-                    $($(".class-intro")[i]).text(courses[i].introduce);
-                    $($(".img-responsive")[i]).attr("src","http://115.159.188.200:8000"+courses[i].cover);
-                    
-                };
-            },
-            //error:function(XMLHttpRequest, textStatus, errorThrown){
-            //通常情况下textStatus和errorThrown只有其中一个包含信息
-            error: function(XMLHttpRequest, textStatus, errorThrown) {
-                window.alert(textStatus);
-            }
-    });
-     
-    //监听所有的课程封面及课程名 
+    initCourse();
+    //监听所有的课程封面及课程名
     $(".class-pic").on("click", function(event){
         var $this = $(this);
         var id = $this.attr("course-id");
@@ -67,3 +31,24 @@ $(function() {
 
 })
 
+function initCourse() {
+
+    //连接服务器获取课程列表
+    var limit = 16;
+    var url = "http://115.159.188.200:8000/get_courses/";
+    var myPage = new myPaginate(limit, url, callb);
+    myPage.init();
+
+    function callb(data) {
+        console.log(data);
+        var courses = data.courses;
+        for (var i=0; i<courses.length; i++) {
+            $($(".sed-md")[i]).css("display", "inline");
+            $($(".class-name")[i]).text(courses[i].name);
+            $($(".class-name")[i]).attr("course-id",courses[i].id);
+            $($(".class-pic")[i]).attr("course-id",courses[i].id);
+            $($(".class-intro")[i]).text(courses[i].introduce);
+            $($(".img-responsive")[i]).attr("src","http://115.159.188.200:8000"+courses[i].cover);
+        };
+    }
+}
