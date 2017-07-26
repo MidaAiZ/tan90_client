913daf3133b61216fe55606686f4f42353bc1eda
DaddySong <934889998@qq.com>
2017-07-26 15:07:36 +0800

    添加打开关闭lesson页面的post

--- view/js/lesson.js
+++ view/js/lesson.js
@@ -7,6 +7,66 @@ $(function() {
         if(r!=null)return  decodeURI(r[2]); return null;
     }
 
+    //获取页面关闭事件
+    window.onbeforeunload = function(){
+        console.log('关闭页面');
+        $.ajax({
+                type: 'POST',
+                url: 'http://115.159.188.200:8000/exit_course/',
+                dataType: 'json',
+                data: 'course_id=' + GetQueryString('course_id'),
+                //下面2个参数用于解决跨域问题
+                xhrFields: {
+                    withCredentials: true
+                },
+                crossDomain: true,
+                complete: function(XMLHttpRequest, textStatus) {
+                },
+                success: function(data) {
+                    if(data.code==1000){
+                        console.log("post开始时间成功。");
+                    }else{
+                        console.log(data.msg);
+                    }
+                },
+                //error:function(XMLHttpRequest, textStatus, errorThrown){
+                //通常情况下textStatus和errorThrown只有其中一个包含信息
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    window.alert(textStatus);
+                }
+        });
+        return "hhhhhh确认退出?";
+    }
+    window.onload = function(){
+        console.log('打开页面');
+        $.ajax({
+                type: 'POST',
+                url: 'http://115.159.188.200:8000/watch_course/',
+                dataType: 'json',
+                data: 'course_id=' + GetQueryString('course_id'),
+                //下面2个参数用于解决跨域问题
+                xhrFields: {
+                    withCredentials: true
+                },
+                crossDomain: true,
+                complete: function(XMLHttpRequest, textStatus) {
+                },
+                success: function(data) {
+                    if(data.code==1000){
+                        console.log("post开始时间成功。");
+                    }else{
+                        console.log(data.msg);
+                    }
+                },
+                //error:function(XMLHttpRequest, textStatus, errorThrown){
+                //通常情况下textStatus和errorThrown只有其中一个包含信息
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    window.alert(textStatus);
+                }
+        });
+        return "打开了";
+    }
+
     //计算数组长度
     function count(str){
         var s = typeof str;
@@ -20,7 +80,7 @@ $(function() {
                 }
                 return i;
             }
-        }   
+        }
     }
 
 
@@ -35,49 +95,51 @@ $(function() {
     //设置catalog数组
     function setCatalogArray(data){
         //将返回数据存入catalog数组
-        for (var i = 0; i < data.chapters.length; i++) {
-                chapsname[i] = (data.chapters[i]).name;
-                var chapterid = (data.chapters[i]).id;
-                var chaptername = (data.chapters[i]).name;
-                var chapterindex = (data.chapters[i]).index;
+        if(data.code==1000){
+            for (var i = 0; i < data.chapters.length; i++) {
+                  chapsname[i] = (data.chapters[i]).name;
+                  var chapterid = (data.chapters[i]).id;
+                  var chaptername = (data.chapters[i]).name;
+                  var chapterindex = (data.chapters[i]).index;
 
 
-                $.ajax({  
-                    type: 'POST',  
-                    url: "http://115.159.188.200:8000/get_section/",  
-                    dataType: 'json',  
-                    data: "chapter_id="+chapterid,
-                    //下面2个参数用于解决跨域问题  
-                    xhrFields: {
-                        withCredentials: true
-                    },
-                    crossDomain: true,
-                    async: false,
-                    complete: function(XMLHttpRequest, textStatus) { 
-                    },  
-                    success: function(data) { 
-                        console.log(data); 
-                        for (var i = 0; i < data.sections.length; i++) {
-                            catalog[t] = {"chapterId": chapterid,
-                                "chapterIndex": chapterindex,
-                                "chapterName": chaptername,
-                                "sectionId":(data.sections[i]).id,
-                                "sectionIndex":(data.sections[i]).index, 
-                                "sectionName":(data.sections[i]).name
-                            };
-                            t++;
-                        };
-                        
-                    },
-                    //error:function(XMLHttpRequest, textStatus, errorThrown){
-                    //通常情况下textStatus和errorThrown只有其中一个包含信息
-                    error: function(XMLHttpRequest, textStatus, errorThrown) {
-                        window.alert(textStatus);
-                    }
-                });
+                  $.ajax({
+                      type: 'POST',
+                      url: "http://115.159.188.200:8000/get_section/",
+                      dataType: 'json',
+                      data: "chapter_id="+chapterid,
+                      //下面2个参数用于解决跨域问题
+                      xhrFields: {
+                          withCredentials: true
+                      },
+                      crossDomain: true,
+                      async: false,
+                      complete: function(XMLHttpRequest, textStatus) {
+                      },
+                      success: function(data) {
+                          console.log(data);
+                          for (var i = 0; i < data.sections.length; i++) {
+                              catalog[t] = {"chapterId": chapterid,
+                                  "chapterIndex": chapterindex,
+                                  "chapterName": chaptername,
+                                  "sectionId":(data.sections[i]).id,
+                                  "sectionIndex":(data.sections[i]).index,
+                                  "sectionName":(data.sections[i]).name
+                              };
+                              t++;
+                          };
 
-        };
-        
+                      },
+                      //error:function(XMLHttpRequest, textStatus, errorThrown){
+                      //通常情况下textStatus和errorThrown只有其中一个包含信息
+                      error: function(XMLHttpRequest, textStatus, errorThrown) {
+                          window.alert(textStatus);
+                      }
+                  });
+
+            };
+        }
+
     }
 
     //动态生成课程目录
@@ -89,6 +151,7 @@ $(function() {
                 $(".img-responsive").attr('src','http://115.159.188.200:8000'+data.cover);
 
                 var chaps = data.chapters;
+                if(data.code==1000){
                 for (var i=0; i<data.chapters.length;i++){
                     $("#chapter").append('<h3>第'+chaps[i].index+'章 '+chaps[i].name+'</h3>');
                     var $list = $('<ul></ul>');
@@ -107,30 +170,32 @@ $(function() {
                     };
                 };
 
+
                 $(".part").on("click", function(event){
                     var $this = $(this);
                     window.location.href='lesson.html?course_id='+GetQueryString("course_id")+'&chapter_index='+$this.attr("chapter-index")+'&lesson_index='+$this.attr("section-index");
                 });
+                }
     }
 
 
     //连接服务器获取课程详细内容
     var params = "id="+ GetQueryString("course_id");
-    var url = "http://115.159.188.200:8000/get_chapter/"; 
-    $.ajax({  
-            type: 'POST',  
-            url: url,  
-            dataType: 'json',  
+    var url = "http://115.159.188.200:8000/get_chapter/";
+    $.ajax({
+            type: 'POST',
+            url: url,
+            dataType: 'json',
             data: params,
-            //下面2个参数用于解决跨域问题  
+            //下面2个参数用于解决跨域问题
             xhrFields: {
                 withCredentials: true
             },
             crossDomain: true,
 
-            complete: function(XMLHttpRequest, textStatus) { 
-            },  
-            success: function(data) {  
+            complete: function(XMLHttpRequest, textStatus) {
+            },
+            success: function(data) {
                 console.log(data);
 
                 //设置catalog数组
@@ -144,22 +209,22 @@ $(function() {
                 for (var i=0;i<count(catalog);i++) {
                     if(chapterIdx==catalog[i].chapterIndex&&lessonIdx==catalog[i].sectionIndex){
                         $("#lesson-title").text(chapterIdx+'-'+lessonIdx+' '+catalog[i].sectionName);
-                        
-                        $.ajax({  
-                            type: 'POST',  
-                            url: "http://115.159.188.200:8000/get_content/",  
-                            dataType: 'json',  
+
+                        $.ajax({
+                            type: 'POST',
+                            url: "http://115.159.188.200:8000/get_content/",
+                            dataType: 'json',
                             data: "section_id="+catalog[i].sectionId,
-                            //下面2个参数用于解决跨域问题  
+                            //下面2个参数用于解决跨域问题
                             xhrFields: {
                                 withCredentials: true
                             },
                             crossDomain: true,
                             async: false,
-                            complete: function(XMLHttpRequest, textStatus) { 
-                            },  
-                            success: function(data) { 
-                                console.log(data); 
+                            complete: function(XMLHttpRequest, textStatus) {
+                            },
+                            success: function(data) {
+                                console.log(data);
                                 if(data.code==1000){
                                     var flagV = false;
                                     var flagA = false;
@@ -219,7 +284,7 @@ $(function() {
                                         $video.parentNode.removeChild($video);
                                     }
                                 }
-                                
+
                             },
                             //error:function(XMLHttpRequest, textStatus, errorThrown){
                             //通常情况下textStatus和errorThrown只有其中一个包含信息
@@ -243,18 +308,18 @@ $(function() {
                         if(i==count(catalog)-1){
                             $("#next-lesson").remove();
                         }
-                        
+
 
                         setDiscussion(catalog[i].chapterId);
                     }
-            
+
                 };
-                
+
 
 
                 //动态生成课程内容及目录
                 createCatalogItems(data);
-                
+
             },
             //error:function(XMLHttpRequest, textStatus, errorThrown){
             //通常情况下textStatus和errorThrown只有其中一个包含信息
@@ -263,14 +328,14 @@ $(function() {
             }
     });
 
-    
 
 
-      
+
+
 
     /**
     下面是课程问答模块的自动生成
-    **/ 
+    **/
 
     function setDiscussion(chapterid) {
         var $answInput;
@@ -288,19 +353,19 @@ $(function() {
                       "content": content,
                       "chapter_id":  chapterid
                      }
-        $.ajax({  
-                type: 'POST',  
-                url: 'http://115.159.188.200:8000/post_new_discution/',  
-                dataType: 'json',  
+        $.ajax({
+                type: 'POST',
+                url: 'http://115.159.188.200:8000/post_new_discution/',
+                dataType: 'json',
                 data: params,
-                //下面2个参数用于解决跨域问题  
+                //下面2个参数用于解决跨域问题
                 xhrFields: {
                     withCredentials: true
                 },
                 crossDomain: true,
-                complete: function(XMLHttpRequest, textStatus) { 
-                },  
-                success: function(data) { 
+                complete: function(XMLHttpRequest, textStatus) {
+                },
+                success: function(data) {
                     if(data.code=="1000"){
                         window.location.reload();
                     }
@@ -311,7 +376,7 @@ $(function() {
                     window.alert(textStatus);
                 }
         });
-                        
+
     });
 
     //连接服务器获取讨论区内容
@@ -320,25 +385,25 @@ $(function() {
                     "page_index": 1,
                     "chapter_id":  chapterid
                   };
-    
-    $.ajax({  
-            type: 'POST',  
-            url: "http://115.159.188.200:8000/get_discussion_list/",  
-            dataType: 'json',  
+
+    $.ajax({
+            type: 'POST',
+            url: "http://115.159.188.200:8000/get_discussion_list/",
+            dataType: 'json',
             data: params,
-            //下面2个参数用于解决跨域问题  
+            //下面2个参数用于解决跨域问题
             xhrFields: {
                 withCredentials: true
             },
             crossDomain: true,
 
-            complete: function(XMLHttpRequest, textStatus) { 
-            },  
-            success: function(data) {  
+            complete: function(XMLHttpRequest, textStatus) {
+            },
+            success: function(data) {
                 console.log(data);
 
                 var discList = data.discussion_list;
-                
+
                 for (var i = 0; i < discList.length; i++) {
 
                     var $quesAnsw = $('<div />',{class: 'ques-answer num'+i});
@@ -365,7 +430,7 @@ $(function() {
                     $answInput.append('<input type="text" class="replytext" name="replytext" placeholder="请输入回复内容" />'+
                         '<span diss-id='+discList[i].discussion_id+' class="reply">回复</span>');
                     $("#discussion").append($quesAnsw);
-                    
+
                     $quesAnsw.append($parentDiv);
                     $parentDiv.append($headPic);
                     $parentDiv.append($quesUser);
@@ -383,20 +448,20 @@ $(function() {
                             "content": content,
                             "discussion_id":  $this.attr("diss-id")
                         }
-                        $.ajax({  
-                                type: 'POST',  
-                                url: 'http://115.159.188.200:8000/post_reply/',  
-                                dataType: 'json',  
+                        $.ajax({
+                                type: 'POST',
+                                url: 'http://115.159.188.200:8000/post_reply/',
+                                dataType: 'json',
                                 data: params,
-                                //下面2个参数用于解决跨域问题  
+                                //下面2个参数用于解决跨域问题
                                 xhrFields: {
                                     withCredentials: true
                                 },
                                 crossDomain: true,
-                    
-                                complete: function(XMLHttpRequest, textStatus) { 
-                                },  
-                                success: function(data) {  
+
+                                complete: function(XMLHttpRequest, textStatus) {
+                                },
+                                success: function(data) {
                                     //console.log(data);
                                     if(data.code=="1000"){
                                         console.log($answCon);
@@ -411,7 +476,7 @@ $(function() {
                                     window.alert(textStatus);
                                 }
                         });
-                        
+
                 });
 
             },
@@ -420,11 +485,11 @@ $(function() {
             error: function(XMLHttpRequest, textStatus, errorThrown) {
                 window.alert(textStatus);
             }
-    }); 
+    });
 
     }
 
-    
+
 
     //侧边栏js
     $('.menu , .linee').on('click', function() {
@@ -433,8 +498,8 @@ $(function() {
         $('.linea2').toggleClass('overL2')
         $('.linea3').toggleClass('overL3')
         $('.main-menu').toggleClass('overmain')
-    });  
-    
+    });
+
 
     //滚动条js
     (function($){
@@ -444,5 +509,5 @@ $(function() {
                 theme:"light-thin"
             });
         });
-    })(jQuery);     
+    })(jQuery);
 });
